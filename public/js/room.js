2b318b1aacc1aa6db739cdd63d4b078c14251f2f
nipun <nipungarg.nipun@gmail.com>
2021-06-29 00:16:59 +0530

    port 443

--- public/js/room.js
+++ public/js/room.js
@@ -87,7 +87,7 @@ function showLater() {
   const myPeer = new Peer(undefined, {
     path: "/peerjs",
     host: "/",
-    port: "3000",
+    port: "443",
     // proxied: true,
     // secure: true,
   });
